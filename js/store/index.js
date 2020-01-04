e44f7162748e0ef063b8be54f511d02df87a6d89
guojiabao <g1079832313@163.com>
2020-01-04 20:27:40 +0800

    添加食物图片

--- js/store/index.js
+++ js/store/index.js
@@ -38,52 +38,68 @@ const store = new Vuex.Store({
           exp: 100,       // 增加小鸡经验
           num: 2,         // 库存数量
           unlock: 1,      // 0为待解锁，1为已解锁
-          unlockPrice: 1  // 解锁金额
+          unlockPrice: 1, // 解锁金额
+          url: 'images/food1.png' // 图片
         },{
           id: 2,
-          name: '香蕉',
+          name: '三叶草',
           price: 100,
           eatTime: 10000,
           exp: 600,
           num: 0,
           unlock: 0,
-          unlockPrice: 100
+          unlockPrice: 100,
+          url: 'images/food2.png'
         },{
           id: 3,
-          name: '玉米',
+          name: '橄榄',
           price: 150,
           eatTime: 120000,
           exp: 800,
           num: 0,
           unlock: 0,
-          unlockPrice: 500
+          unlockPrice: 500,
+          url: 'images/food3.png'
         },{
           id: 4,
-          name: '苹果',
+          name: '面包',
           price: 450,
           eatTime: 160000,
           exp: 1000,
           num: 0,
           unlock: 0,
-          unlockPrice: 1000
+          unlockPrice: 1000,
+          url: 'images/food4.png'
         },{
           id: 5,
-          name: '榴莲',
+          name: '葡萄',
           price: 650,
           eatTime: 170000,
           exp: 1100,
           num: 0,
           unlock: 0,
-          unlockPrice: 1300
+          unlockPrice: 1300,
+          url: 'images/food5.png'
         },{
           id: 6,
-          name: '车厘子',
+          name: '豆腐',
           price: 750,
           eatTime: 180000,
           exp: 1200,
           num: 0,
           unlock: 0,
-          unlockPrice: 1500
+          unlockPrice: 1500,
+          url: 'images/food6.png'
+        },{
+          id: 7,
+          name: '大包菜',
+          price: 750,
+          eatTime: 180000,
+          exp: 1200,
+          num: 0,
+          unlock: 0,
+          unlockPrice: 1500,
+          url: 'images/food7.png'
         }],
         startDate: '',    // 开始时间
         endDate: '',      // 结束时间
