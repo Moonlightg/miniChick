29f7f3b5eff545b2833a1c478e75d725e73a606b
guojiabao <g1079832313@163.com>
2020-01-23 22:53:44 +0800

    优化学习课程

--- js/store/subject.js
+++ js/store/subject.js
@@ -6,51 +6,63 @@ var subjects = [{
   list: [{
     id: 10011,
     name: '一年级(上册)',
-    img: 'images/subject/AA1.jpg'
+    img: 'images/subject/AA1.jpg',
+    learning: 1 // 0:未学习，1:学习中，2:已毕业
   },{
     id: 10012,
     name: '一年级(下册)',
-    img: 'images/subject/AA2.jpg'
+    img: 'images/subject/AA2.jpg',
+    learning: 0 
   },{
     id: 10021,
     name: '二年级(上册)',
-    img: 'images/subject/AB1.jpg'
+    img: 'images/subject/AB1.jpg',
+    learning: 0 
   },{
     id: 10022,
     name: '二年级(下册)',
-    img: 'images/subject/AB2.jpg'
+    img: 'images/subject/AB2.jpg',
+    learning: 0 
   },{
     id: 10031,
     name: '三年级(上册)',
-    img: 'images/subject/AC1.jpg'
+    img: 'images/subject/AC1.jpg',
+    learning: 0 
   },{
     id: 10032,
     name: '三年级(下册)',
-    img: 'images/subject/AC2.jpg'
+    img: 'images/subject/AC2.jpg',
+    learning: 0 
   },{
     id: 10041,
     name: '四年级(上册)',
-    img: 'images/subject/AD1.jpg'
+    img: 'images/subject/AD1.jpg',
+    learning: 0 
   },{
     id: 10042,
     name: '四年级(下册)',
-    img: 'images/subject/AD2.jpg'
+    img: 'images/subject/AD2.jpg',
+    learning: 0 
   },{
     id: 10051,
     name: '五年级(上册)',
-    img: 'images/subject/AE1.jpg'
+    img: 'images/subject/AE1.jpg',
+    learning: 0 
   },{
     id: 10052,
     name: '五年级(下册)',
-    img: 'images/subject/AE2.jpg'
+    img: 'images/subject/AE2.jpg',
+    learning: 0 
   },{
     id: 10061,
     name: '六年级(上册)',
-    img: 'images/subject/AF1.jpg'
+    img: 'images/subject/AF1.jpg',
+    learning: 0 
   },{
     id: 10062,
     name: '六年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/AF2.jpg',
+    learning: 0 
   }]
 },{
   id: 2,
@@ -59,50 +71,62 @@ var subjects = [{
   list: [{
     id: 20011,
     name: '一年级(上册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BA1.jpg',
+    learning: 0 
   },{
     id: 20012,
     name: '一年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BA2.jpg',
+    learning: 0 
   },{
     id: 20021,
     name: '二年级(上册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BB1.jpg',
+    learning: 0 
   },{
     id: 20022,
     name: '二年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BB2.jpg',
+    learning: 0 
   },{
     id: 20031,
     name: '三年级(上册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BC1.jpg',
+    learning: 0 
   },{
     id: 20032,
     name: '三年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BC2.jpg',
+    learning: 0 
   },{
     id: 20041,
     name: '四年级(上册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BD1.jpg',
+    learning: 0 
   },{
     id: 20042,
     name: '四年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BD2.jpg',
+    learning: 0 
   },{
     id: 20051,
     name: '五年级(上册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BE1.jpg',
+    learning: 0 
   },{
     id: 20052,
     name: '五年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BE2.jpg',
+    learning: 0 
   },{
     id: 20061,
     name: '六年级(上册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BF1.jpg',
+    learning: 0 
   },{
     id: 20062,
     name: '六年级(下册)',
-    img: 'images/subject/AF2.jpg'
+    img: 'images/subject/BF2.jpg',
+    learning: 0 
   }]
 }]
